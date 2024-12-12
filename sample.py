5d1b854d06a63c1cc3aceb7f0781e8ab59e9d06d
fijupepa <filipe.honras@gmail.com>
2024-12-11 23:45:52 -0300

    5
    5

--- sample.py
+++ sample.py
@@ -29,4 +29,4 @@ def get_user():
 
 if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
-    app.run(debug=debug_mode)
+    app.run(debug=True)
